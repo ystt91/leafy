45cae10913fe2327f0757eb32bb90fc553f47f91
TYS <poiu62875@gmail.com>
2025-01-16 19:39:11 +0900

    #공백 추가

--- leafy-frontend/src/App.vue
+++ leafy-frontend/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div class="welcome-message" v-if="isLoggedIn">
       <p class="welcome-text">안녕하세요, <span class="user-name">{{ user.name }}</span>님!</p>
-      <p class="description">왜 안되죠??</p>
+      <p class="description">왜 안되죠?? </p>
     </div>
     <div class="brand" v-if="isLoggedIn">
       LEAFY
